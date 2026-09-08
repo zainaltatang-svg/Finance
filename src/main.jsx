3326264daff90c21e08f116ddaf62bbbe7b33f81
zenal tatang <zainaltatang@gmail.com>
2026-09-08 19:20:12 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "../JSX/strimnet-manajemen-bisnis.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
